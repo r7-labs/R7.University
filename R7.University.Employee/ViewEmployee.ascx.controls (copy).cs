8b3a3f66e0a4c5002494ccec02ecd9bacd655f88
roman-yagodin <roman.yagodin@gmail.com>
2014-04-29 16:02:45 +0400

    Add ViewVCard pre-template

---
+++ R7.University.Employee/ViewEmployee.ascx.controls (copy).cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Linq;
+using DotNetNuke.UI.UserControls;
+using DotNetNuke.UI.WebControls;
+using R7.University;
+
+namespace R7.University.Employee
+{
+	public partial class ViewEmployee
+	{
+		protected Panel panelEmployee;
+		protected Image imagePhoto;
+		protected Label labelFullName;
+		protected Label labelPositions;
+		protected Label labelAcademicDegreeAndTitle;
+		// protected Label labelAcademicTitle;
+		// protected Label labelAcademicDegree;
+		protected Label labelPhone;
+		protected Label labelCellPhone;
+		protected Label labelFax;
+		protected Label labelMessenger;
+		//protected Label labelWorkingHours;
+		//protected Label labelWorkingPlace;
+		protected Label labelWorkingPlaceAndHours;
+
+		protected HyperLink linkWebSite;
+		protected HyperLink linkEmail;
+		protected HyperLink linkSecondaryEmail;
+		protected HyperLink linkUserProfile;
+		protected Repeater repeaterPositions;
+	}
+}
