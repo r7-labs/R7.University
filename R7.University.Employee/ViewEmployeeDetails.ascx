639a5a715d809bb4cb6e3bae1b193d4075d36a87
roman-yagodin <roman.yagodin@gmail.com>
2014-05-05 12:29:00 +0400

    Rename template control to ViewEmployeeDetails

---
+++ R7.University.Employee/ViewEmployeeDetails.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ViewEmployeeDetails.ascx.cs" Inherits="R7.University.Employee.ViewEmployeeDetails" %>
---
+++ R7.University.Employee/ViewEmployeeDetails.ascx.controls.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Linq;
+using DotNetNuke.UI.UserControls;
+using DotNetNuke.UI.WebControls;
+using R7.University;
+
+namespace R7.University.Employee
+{
+	public partial class ViewEmployeeDetails
+	{
+	}
+}
