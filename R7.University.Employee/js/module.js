5d8fee8c4991efa131123e41af9780fb1645295a
roman-yagodin <roman.yagodin@gmail.com>
2015-04-17 13:16:31 +0400

    Update dnnGrid paint script to work on tables with or without tbody tags

--- R7.University.Employee/js/module.js
+++ R7.University.Employee/js/module.js
@@ -2,8 +2,11 @@
     // paint tables with dnnGrid classes
     var table = ".employeeDetails #employeeDisciplines table";
     $(table).addClass ("dnnGrid").css ("border-collapse", "collapse");
-    // use parent () to get rows with matched cell types
-    $(table + " tr:nth-child(even) td").parent ().addClass ("dnnGridItem")
+    // with tbody
+    $(table + " tbody tr:nth-child(odd) td").parent ().addClass ("dnnGridItem")
+        .next ().addClass ("dnnGridAltItem");
+    // without tbody
+    $(table + " > tr:nth-child(even) td").parent ().addClass ("dnnGridItem")
         .next ().addClass ("dnnGridAltItem");
     // paint headers
     $(table + " tr th").parent ().addClass ("dnnGridHeader").attr ("align", "left");
