39844c7912ed1be442db795ce7b8a13842cd98fb
roman-yagodin <roman.yagodin@gmail.com>
2015-11-18 16:40:36 +0300

    Add IsPublished property to DivisionInfo model

--- R7.University/Models/DivisionInfo.cs
+++ R7.University/Models/DivisionInfo.cs
@@ -123,6 +123,16 @@ namespace R7.University
 
 		#endregion
 
+        [IgnoreColumn]
+        public bool IsPublished
+        {
+            get
+            {
+                var now = DateTime.Now;
+                return (StartDate == null || now >= StartDate) && (EndDate == null || now < EndDate);
+            }
+        }
+
 		[IgnoreColumn]
 		public string FileName
 		{
