ac3a984988db7f4ecbb7bc62354c0f37abf5ab11
roman-yagodin <roman.yagodin@gmail.com>
2016-01-11 14:27:55 +0300

    Remove unused POCO class and IPortable implementation - also fixes coverity defect

--- R7.University.Launchpad/components/LaunchpadController.cs
+++ R7.University.Launchpad/components/LaunchpadController.cs
@@ -13,7 +13,7 @@ using R7.University;
 
 namespace R7.University.Launchpad
 {
-	public partial class LaunchpadController : UniversityControllerBase, IPortable
+	public partial class LaunchpadController : UniversityControllerBase
 	{
 		#region Public methods
 
@@ -43,59 +43,6 @@ namespace R7.University.Launchpad
 		}
 
 		#endregion
-
-		#region IPortable implementation
-
-		/// <summary>
-		/// Exports a module to XML
-		/// </summary>
-		/// <param name="ModuleID">a module ID</param>
-		/// <returns>XML string with module representation</returns>
-		public string ExportModule (int moduleId)
-		{
-			var sb = new StringBuilder ();
-			var infos = GetObjects<LaunchpadInfo> (moduleId);
-
-            if (infos.Any ())
-			{
-				sb.Append ("<Launchpads>");
-				foreach (var info in infos)
-				{
-					sb.Append ("<Launchpad>");
-					sb.Append ("<content>");
-					sb.Append (XmlUtils.XMLEncode (info.Content));
-					sb.Append ("</content>");
-					sb.Append ("</Launchpad>");
-				}
-				sb.Append ("</Launchpads>");
-			}
-			
-			return sb.ToString ();
-		}
-
-		/// <summary>
-		/// Imports a module from an XML
-		/// </summary>
-		/// <param name="ModuleID"></param>
-		/// <param name="Content"></param>
-		/// <param name="Version"></param>
-		/// <param name="UserID"></param>
-		public void ImportModule (int ModuleID, string Content, string Version, int UserID)
-		{
-			var infos = DotNetNuke.Common.Globals.GetContent (Content, "Launchpads");
-		
-			foreach (XmlNode info in infos.SelectNodes("Launchpad"))
-			{
-				var item = new LaunchpadInfo ();
-				item.ModuleID = ModuleID;
-				item.Content = info.SelectSingleNode ("content").InnerText;
-				item.CreatedByUser = UserID;
-
-				Add<LaunchpadInfo> (item);
-			}
-		}
-
-		#endregion
-	}
+    }
 }
 
