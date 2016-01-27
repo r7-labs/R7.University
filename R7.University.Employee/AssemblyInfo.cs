a655afd5478a275c054ba9f2578068453bbe4600
roman-yagodin <roman.yagodin@gmail.com>
2016-01-27 15:17:03 +0300

    Format AssemblyInfo files, remove license headers

--- R7.University.Employee/AssemblyInfo.cs
+++ R7.University.Employee/AssemblyInfo.cs
@@ -19,4 +19,3 @@ using System.Runtime.CompilerServices;
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
-
