ec9564dcdb433f8deb6afc862deaf823366e78e3
roman-yagodin <roman.yagodin@gmail.com>
2016-04-29 08:59:11 +0300

    Remove DotNetNuke.R7.dll from local deploy

--- R7.University/Data/UniversityRepository.cs
+++ R7.University/Data/UniversityRepository.cs
@@ -1,22 +1,40 @@
-﻿using System;
+﻿//
+// UniversityControllerBase.cs
+//
+// Author:
+//       Roman M. Yagodin <roman.yagodin@gmail.com>
+//
+// Copyright (c) 2015-2016 Roman M. Yagodin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
 using System.Data;
 using System.Collections.Generic;
-using System.Text;
-using System.Xml;
 using System.Linq;
-using DotNetNuke.Collections;
 using DotNetNuke.Data;
-using DotNetNuke.Common.Utilities;
-using DotNetNuke.Entities.Modules;
-using DotNetNuke.Services.Search;
-using DotNetNuke.Entities.Tabs;
-using DotNetNuke.Web.UI.WebControls;
-using DotNetNuke.R7;
 using R7.University;
+using R7.DotNetNuke.Extensions.Data;
 
 namespace R7.University
 {
-    public abstract class UniversityControllerBase: ControllerBase
+    public abstract class UniversityControllerBase: Dal2DataProvider
 	{
 		#region Custom methods
 
