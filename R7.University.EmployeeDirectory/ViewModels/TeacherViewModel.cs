2b9a457f476e7ed208576a3871f93c2f94f0c00d
roman-yagodin <roman.yagodin@gmail.com>
2016-06-14 11:51:32 +0300

    Fix #123
    [EmployeeDirectory] Education and Trainig columns show same content

--- R7.University.EmployeeDirectory/ViewModels/TeacherViewModel.cs
+++ R7.University.EmployeeDirectory/ViewModels/TeacherViewModel.cs
@@ -305,7 +305,7 @@ namespace R7.University.EmployeeDirectory.ViewModels
         {
             get {
                 return TextUtils.FormatList ("; ", Model.Achievements
-                    .Where (ach => ach.AchievementType == AchievementType.Education)
+                    .Where (ach => ach.AchievementType == AchievementType.Training)
                     .Select (ach => TextUtils.FormatList ("&nbsp;- ", 
                         FormatHelper.FormatShortTitle (ach.ShortTitle, ach.Title, ach.TitleSuffix), ach.YearBegin))
                 );
