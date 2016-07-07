247ae865bf72f0a40136625ea3ffe967fb430394
roman-yagodin <roman.yagodin@gmail.com>
2016-07-07 16:13:03 +0300

    Switch to model context for EditEduForm

--- R7.University.Launchpad/EditDocumentType.cs
+++ R7.University.Launchpad/EditDocumentType.cs
@@ -20,7 +20,6 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using R7.DotNetNuke.Extensions.Modules;
-using R7.University.Data;
 using R7.University.Models;
 
 namespace R7.University.Launchpad
