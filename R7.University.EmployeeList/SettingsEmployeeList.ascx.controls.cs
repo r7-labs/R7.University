178c26882153637227df87df0939739e5a8bf2f0
roman-yagodin <roman.yagodin@gmail.com>
2016-09-29 13:40:41 +0300

    Use new DivisionSelector control in EmployeeList settings form

--- R7.University.EmployeeList/SettingsEmployeeList.ascx.controls.cs
+++ R7.University.EmployeeList/SettingsEmployeeList.ascx.controls.cs
@@ -1,18 +1,11 @@
-﻿using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.HtmlControls;
-using DotNetNuke.UI.UserControls;
-using DotNetNuke.UI.WebControls;
-using DotNetNuke.Web.UI.WebControls;
-using R7.University;
+﻿using System.Web.UI.WebControls;
+using R7.University.Controls;
 
 namespace R7.University.EmployeeList
 {
     public partial class SettingsEmployeeList
     {
-        protected LabelControl labelDivision;
-        protected DnnTreeView treeDivisions;
+        protected DivisionSelector divisionDivision;
         protected CheckBox checkIncludeSubdivisions;
         protected CheckBox checkHideHeadEmployee;
         protected DropDownList comboSortType;
