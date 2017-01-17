ee589b57b5c7d18e24b00c5bdd962cda626d2dc0
roman-yagodin <roman.yagodin@gmail.com>
2017-01-17 13:20:15 +0300

    Add AsEnumerable() extension method to ListItemCollection

--- R7.University/ControlExtensions/ListControlExtensions.cs
+++ R7.University/ControlExtensions/ListControlExtensions.cs
@@ -4,7 +4,7 @@
 //  Author:
 //       Roman M. Yagodin <roman.yagodin@gmail.com>
 //
-//  Copyright (c) 2016 Roman M. Yagodin
+//  Copyright (c) 2016-2017 Roman M. Yagodin
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Affero General Public License as published by
@@ -20,6 +20,7 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
+using System.Collections.Generic;
 using System.Web.UI.WebControls;
 
 namespace R7.University.ControlExtensions
@@ -48,6 +49,14 @@ namespace R7.University.ControlExtensions
         public static void InsertDefaultItem (this ListControl listControl, string text, int value = -1)
         {
             listControl.Items.Insert (0, new ListItem (text, value.ToString ()));
+        }
+
+        public static IEnumerable<ListItem> AsEnumerable (this ListItemCollection itemCollection)
+        {
+            var enumerator = itemCollection.GetEnumerator ();
+            while (enumerator.MoveNext ()) {
+                yield return (ListItem) enumerator.Current;
+            }
         }
     }
 }
