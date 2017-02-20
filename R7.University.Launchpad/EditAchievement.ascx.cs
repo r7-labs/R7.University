1ed01cbf5115eb571f366d1c5c29a498ec23bb34
roman-yagodin <roman.yagodin@gmail.com>
2017-02-20 12:06:48 +0300

    Inherit EditAchievement from EditPortalModuleBase

--- R7.University.Launchpad/EditAchievement.ascx.cs
+++ R7.University.Launchpad/EditAchievement.ascx.cs
@@ -4,7 +4,7 @@
 //  Author:
 //       Roman M. Yagodin <roman.yagodin@gmail.com>
 //
-//  Copyright (c) 2014-2016 Roman M. Yagodin
+//  Copyright (c) 2014-2017 Roman M. Yagodin
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Affero General Public License as published by
@@ -20,21 +20,14 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
-using DotNetNuke.Common;
-using DotNetNuke.Entities.Modules;
-using DotNetNuke.Services.Exceptions;
-using DotNetNuke.Services.Localization;
 using R7.DotNetNuke.Extensions.ControlExtensions;
-using R7.DotNetNuke.Extensions.Utilities;
+using R7.DotNetNuke.Extensions.Modules;
 using R7.University.Models;
-using R7.University.Utilities;
 
 namespace R7.University.Launchpad
 {
-    public partial class EditAchievement : PortalModuleBase
+    public partial class EditAchievement : EditPortalModuleBase<AchievementInfo, int>
     {
-        private int? itemId = null;
-
         #region Model context
 
         private UniversityModelContext modelContext;
@@ -54,141 +47,60 @@ namespace R7.University.Launchpad
 
         #endregion
 
-        #region Handlers
+        protected EditAchievement () : base ("achievement_id")
+        {
+        }
 
-        /// <summary>
-        /// Handles Init event for a control.
-        /// </summary>
-        /// <param name="e">Event args.</param>
+        protected override void InitControls ()
+        {
+            InitControls (buttonUpdate, buttonDelete, linkCancel);
+        }
+
         protected override void OnInit (EventArgs e)
         {
             base.OnInit (e);
 
-            // set url for Cancel link
-            linkCancel.NavigateUrl = UrlHelper.GetCancelUrl (UrlHelper.IsInPopup (Request));
-
-            // add confirmation dialog to delete button
-            buttonDelete.Attributes.Add (
-                "onClick",
-                "javascript:return confirm('" + Localization.GetString ("DeleteItem") + "');");
-
-            // bind achievement types
             comboAchievementType.DataSource = AchievementTypeInfo.GetLocalizedAchievementTypes (LocalizeString);
             comboAchievementType.DataBind ();
         }
 
-        /// <summary>
-        /// Handles Load event for a control.
-        /// </summary>
-        /// <param name="e">Event args.</param>
-        protected override void OnLoad (EventArgs e)
+        protected override void LoadItem (AchievementInfo item)
         {
-            base.OnLoad (e);
-			
-            try {
-                // parse querystring parameters
-                itemId = TypeUtils.ParseToNullable<int> (Request.QueryString ["achievement_id"]);
-
-                if (!IsPostBack) {
-                    // load the data into the control the first time we hit this page
-
-                    // check we have an item to lookup
-                    // ALT: if (!Null.IsNull (itemId) 
-                    if (itemId.HasValue) {
-
-                        // load the item
-                        var item = ModelContext.Get<AchievementInfo> (itemId.Value);
-
-                        if (item != null) {
-                            textTitle.Text = item.Title;
-                            textShortTitle.Text = item.ShortTitle;
-                            comboAchievementType.SelectByValue (item.AchievementType);
-                        }
-                        else
-                            Response.Redirect (Globals.NavigateURL (), true);
-                    }
-                    else {
-                        buttonDelete.Visible = false;
-                    }
-                }
-            }
-            catch (Exception ex) {
-                Exceptions.ProcessModuleLoadException (this, ex);
-            }
+            textTitle.Text = item.Title;
+            textShortTitle.Text = item.ShortTitle;
+            comboAchievementType.SelectByValue (item.AchievementType);
         }
 
-        /// <summary>
-        /// Handles Click event for Update button
-        /// </summary>
-        /// <param name='sender'>
-        /// Sender.
-        /// </param>
-        /// <param name='e'>
-        /// Event args.
-        /// </param>
-        protected void buttonUpdate_Click (object sender, EventArgs e)
+        protected override void BeforeUpdateItem (AchievementInfo item)
         {
-            try {
-                AchievementInfo item;
-
-                // determine if we are adding or updating
-                // ALT: if (Null.IsNull (itemId))
-                if (!itemId.HasValue) {
-                    // add new record
-                    item = new AchievementInfo ();
-                }
-                else {
-                    // update existing record
-                    item = ModelContext.Get<AchievementInfo> (itemId.Value);
-                }
-
-                // fill the object
-                item.Title = textTitle.Text.Trim ();
-                item.ShortTitle = textShortTitle.Text.Trim ();
-                item.AchievementType = (AchievementType) Enum.Parse (typeof (AchievementType), comboAchievementType.SelectedValue);
-
-                if (!itemId.HasValue) {
-                    ModelContext.Add<AchievementInfo> (item);
-                }
-                else {
-                    ModelContext.Update<AchievementInfo> (item);
-                }
-
-                ModelContext.SaveChanges ();
-
-                ModuleController.SynchronizeModule (ModuleId);
-
-                Response.Redirect (Globals.NavigateURL (), true);
-            }
-            catch (Exception ex) {
-                Exceptions.ProcessModuleLoadException (this, ex);
-            }
+            item.Title = textTitle.Text.Trim ();
+            item.ShortTitle = textShortTitle.Text.Trim ();
+            item.AchievementType = (AchievementType) Enum.Parse (typeof (AchievementType), comboAchievementType.SelectedValue);
         }
 
-        /// <summary>
-        /// Handles Click event for Delete button
-        /// </summary>
-        /// <param name='sender'>
-        /// Sender.
-        /// </param>
-        /// <param name='e'>
-        /// Event args.
-        /// </param>
-        protected void buttonDelete_Click (object sender, EventArgs e)
-        {
-            try {
-                if (itemId.HasValue) {
-                    
-                    var item = ModelContext.Get<AchievementInfo> (itemId.Value);
-                    ModelContext.Remove<AchievementInfo> (item);
-                    ModelContext.SaveChanges ();
+        #region implemented abstract members of EditPortalModuleBase
 
-                    Response.Redirect (Globals.NavigateURL (), true);
-                }
-            }
-            catch (Exception ex) {
-                Exceptions.ProcessModuleLoadException (this, ex);
-            }
+        protected override AchievementInfo GetItem (int itemId)
+        {
+            return ModelContext.Get<AchievementInfo> (itemId);
+        }
+
+        protected override void AddItem (AchievementInfo item)
+        {
+            ModelContext.Add (item);
+            ModelContext.SaveChanges ();
+        }
+
+        protected override void UpdateItem (AchievementInfo item)
+        {
+            ModelContext.Update (item);
+            ModelContext.SaveChanges ();
+        }
+
+        protected override void DeleteItem (AchievementInfo item)
+        {
+            ModelContext.Remove (item);
+            ModelContext.SaveChanges ();
         }
 
         #endregion
