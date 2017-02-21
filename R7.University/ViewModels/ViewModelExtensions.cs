40fbab7310bf4aee3b85e0f22207dfab4eaaf239
roman-yagodin <roman.yagodin@gmail.com>
2017-02-21 14:38:18 +0300

    Add IViewModel interface with SetContext extension method

---
+++ R7.University/ViewModels/ViewModelExtensions.cs
@@ -0,0 +1,38 @@
+﻿//
+//  ViewModelExtensions.cs
+//
+//  Author:
+//       Roman M. Yagodin <roman.yagodin@gmail.com>
+//
+//  Copyright (c) 2017 Roman M. Yagodin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Collections.Generic;
+using R7.DotNetNuke.Extensions.ViewModels;
+
+namespace R7.University.ViewModels
+{
+    public static class ViewModelExtensions
+    {
+        public static IEnumerable<IViewModel> SetContext (this IEnumerable<IViewModel> viewModels, ViewModelContext context)
+        {
+            foreach (var viewModel in viewModels) {
+                viewModel.SetContext (context);
+            }
+
+            return viewModels;
+        }
+    }
+}
