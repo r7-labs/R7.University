7b33bb838e14a0a1f884b966fc4a057bc5b5d123
roman-yagodin <roman.yagodin@gmail.com>
2017-02-27 12:35:33 +0300

    Add edit control for achievement type

---
+++ R7.University.Launchpad/EditAchievementType.ascx
@@ -0,0 +1,36 @@
+<%@ Control Language="C#" AutoEventWireup="false" CodeBehind="EditDocumentType.ascx.cs" Inherits="R7.University.Launchpad.EditAchievementType" %>
+<%@ Register TagPrefix="dnn" TagName="Label" Src="~/controls/labelcontrol.ascx" %>
+<%@ Register TagPrefix="dnn" Namespace="DotNetNuke.Web.Client.ClientResourceManagement" Assembly="DotNetNuke.Web.Client" %>
+<%@ Register TagPrefix="controls" TagName="AgplSignature" Src="~/DesktopModules/R7.University/R7.University/Controls/AgplSignature.ascx" %>
+
+<dnn:DnnCssInclude runat="server" FilePath="~/DesktopModules/R7.University/R7.University/css/module.css" />
+
+<div class="dnnForm dnnClear">
+	<fieldset>	
+		<div class="dnnFormItem">
+			<dnn:Label id="labelType" runat="server" ControlName="textType" />
+			<asp:TextBox id="textType" runat="server" MaxLength="64" />
+            <asp:RequiredFieldValidator runat="server" ControlToValidate="textType" Display="Dynamic"
+                CssClass="dnnFormMessage dnnFormError" resourcekey="Type.Required" />
+		</div>
+		<div class="dnnFormItem">
+			<dnn:Label id="labelDescription" runat="server" ControlName="textDescription" />
+			<asp:TextBox id="textDescription" runat="server" TextMode="MultiLine" Rows="3" />
+            <asp:RegularExpressionValidator runat="server"
+                CssClass="dnnFormMessage dnnFormError" resourcekey="Description.MaxLength"
+                ControlToValidate="textDescription" Display="Dynamic"
+                ValidationExpression="[\s\S]{0,255}">
+            </asp:RegularExpressionValidator>
+		</div>
+        <div class="dnnFormItem">
+            <dnn:Label id="labelIsSystem" runat="server" ControlName="checkIsSystem" />
+            <asp:CheckBox id="checkIsSystem" runat="server" Enabled="false" />
+        </div>
+	</fieldset>
+	<ul class="dnnActions dnnClear">
+		<li><asp:LinkButton id="buttonUpdate" runat="server" CssClass="dnnPrimaryAction" ResourceKey="cmdUpdate" CausesValidation="true" /></li>
+		<li><asp:LinkButton id="buttonDelete" runat="server" CssClass="dnnSecondaryAction" ResourceKey="cmdDelete" /></li>
+		<li><asp:HyperLink id="linkCancel" runat="server" CssClass="dnnSecondaryAction" ResourceKey="cmdCancel" /></li>
+	</ul>
+    <controls:AgplSignature runat="server" ShowRule="false" />
+</div>
---
+++ R7.University.Launchpad/EditAchievementType.cs
@@ -0,0 +1,82 @@
+//
+//  EditAchievementType.cs
+//
+//  Author:
+//       Roman M. Yagodin <roman.yagodin@gmail.com>
+//
+//  Copyright (c) 2015-2017 Roman M. Yagodin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using R7.University.Models;
+using R7.University.Modules;
+
+namespace R7.University.Launchpad
+{
+    public partial class EditAchievementType: UniversityEditPortalModuleBase<AchievementTypeInfo>
+    {
+        protected EditAchievementType () : base ("achievementtype_id")
+        {
+        }
+
+        protected override void InitControls ()
+        {
+            InitControls (buttonUpdate, buttonDelete, linkCancel); 
+        }
+
+        protected override void LoadItem (AchievementTypeInfo item)
+        {
+            textType.Text = item.Type;
+            textDescription.Text = item.Description;
+            checkIsSystem.Checked = item.IsSystem;
+
+            // disable textType for system types
+            textType.Enabled = !item.IsSystem;
+        }
+
+        protected override void BeforeUpdateItem (AchievementTypeInfo item)
+        {
+            // don't update Type for system types,
+            // also don't update IsSystem value at all
+
+            if (!item.IsSystem) {
+                item.Type = textType.Text.Trim ();
+            }
+
+            item.Description = textDescription.Text.Trim ();
+        }
+
+        #region Implemented abstract members of UniversityEditPortalModuleBase
+
+        protected override void AddItem (AchievementTypeInfo item)
+        {
+            ModelContext.Add (item);
+            ModelContext.SaveChanges ();
+        }
+
+        protected override void UpdateItem (AchievementTypeInfo item)
+        {
+            ModelContext.Update (item);
+            ModelContext.SaveChanges ();
+        }
+
+        protected override void DeleteItem (AchievementTypeInfo item)
+        {
+            ModelContext.Remove (item);
+            ModelContext.SaveChanges ();
+        }
+
+        #endregion
+    }
+}
