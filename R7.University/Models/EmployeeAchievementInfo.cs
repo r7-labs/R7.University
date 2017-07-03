5e120fdd37274c0c533a231344b35919e752d543
roman-yagodin <roman.yagodin@gmail.com>
2017-07-03 09:55:25 +0300

    fixup

--- R7.University/Models/EmployeeAchievementInfo.cs
+++ R7.University/Models/EmployeeAchievementInfo.cs
@@ -24,7 +24,7 @@ using System;
 namespace R7.University.Models
 {
     [Serializable]
-    public class EmployeeAchievementInfo: IEmployeeAchievement
+    public class EmployeeAchievementInfo: IEmployeeAchievementWritable
     {
         #region IEmployeeAchievement implementation
 
