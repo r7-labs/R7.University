22038902469c2e34f72ed242dec3eb9df8dc67e3
roman-yagodin <roman.yagodin@gmail.com>
2017-07-04 11:01:59 +0300

    Viewmodels for MVC views should be public

--- R7.University.Division/ViewModels/DivisionViewModel.cs
+++ R7.University.Division/ViewModels/DivisionViewModel.cs
@@ -38,7 +38,7 @@ using R7.University.ViewModels;
 
 namespace R7.University.Division.ViewModels
 {
-    internal class DivisionViewModel: IDivision
+    public class DivisionViewModel: IDivision
     {
         protected IDivision Division;
 
