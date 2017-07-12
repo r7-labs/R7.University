07caf54276449d7a002b70b2a62a920bc678dbb4
roman-yagodin <roman.yagodin@gmail.com>
2017-07-12 12:11:08 +0300

    Add plural expressions for most common languages

--- R7.University/Utilities/CultureHelper.cs
+++ R7.University/Utilities/CultureHelper.cs
@@ -4,7 +4,7 @@
 //  Author:
 //       Roman M. Yagodin <roman.yagodin@gmail.com>
 //
-//  Copyright (c) 2016 Roman M. Yagodin
+//  Copyright (c) 2016-2017 Roman M. Yagodin
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Affero General Public License as published by
@@ -26,21 +26,42 @@ namespace R7.University.Utilities
 {
     public static class CultureHelper
     {
+        // TODO: Move to the base library
         public static int GetPlural (int n, CultureInfo culture)
         {
             // http://localization-guide.readthedocs.org/en/latest/l10n/pluralforms.html
 
-            // TODO: Add more languages here
-            if (culture.TwoLetterISOLanguageName == "ru") {
-                // nplurals=3;
+            var lang = culture.TwoLetterISOLanguageName;
+            if (lang == "ru") {
+                // nplurals=3
                 return (n % 10 == 1 && n % 100 != 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2);
             }
 
-            // nplurals=2;
+            if (lang == "pl") {
+                // nplurals=3
+                return (n == 1 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2);
+            }
+
+            if (lang == "zh" || lang == "ja" || lang == "ko") {
+                // nplurals=1
+                return 0;
+            }
+
+            if (lang == "ar") {
+                // nplurals=6
+                return (n == 0 ? 0 : n == 1 ? 1 : n == 2 ? 2 : n % 100 >= 3 && n % 100 <= 10 ? 3 : n % 100 >= 11 ? 4 : 5);
+            }
+
+            if (lang == "fr" || lang == "tr" ||  lang == "fa") {
+                // nplurals=2
+                return (n > 1) ? 1 : 0;
+            }
+
+            // nplurals=2 for en, es, it, pt, hi, bn, ta, ur, ml, nl, ...
             return (n != 1) ? 1 : 0;
         }
 
-        // TODO: Add support for other languages
+        // TODO: Add support for other languages?
 
         #region Russian transliteration table
 
