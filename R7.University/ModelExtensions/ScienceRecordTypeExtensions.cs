d5b4b106266a99574f1ab56735ecf2ecf8310d8d
roman-yagodin <roman.yagodin@gmail.com>
2017-10-25 10:10:11 +0300

    Introduce SystemScienceRecordType enum

---
+++ R7.University/ModelExtensions/ScienceRecordTypeExtensions.cs
@@ -0,0 +1,35 @@
+﻿//
+//  ScienceRecordTypeExtensions.cs
+//
+//  Author:
+//       Roman M. Yagodin <roman.yagodin@gmail.com>
+//
+//  Copyright (c) 2017 Roman M. Yagodin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using R7.University.Models;
+
+namespace R7.University.ModelExtensions
+{
+    public static class ScienceRecordTypeExtensions
+    {
+        public static SystemScienceRecordType GetSystemScienceRecordType (this IScienceRecordType scienceRecordType)
+        {
+            SystemScienceRecordType result;
+            return Enum.TryParse<SystemScienceRecordType> (scienceRecordType.Type, out result) ? result : SystemScienceRecordType.Custom;
+        }
+    }
+}
