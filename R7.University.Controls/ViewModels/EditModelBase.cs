abfbe486962517735e7addd39964ae9b245de7e9
roman-yagodin <roman.yagodin@gmail.com>
2017-11-14 10:28:57 +0300

    [BCH] Move EditModelBase back to Controls project

--- R7.University.Controls/ViewModels/EditModelBase.cs
+++ R7.University.Controls/ViewModels/EditModelBase.cs
@@ -23,8 +23,9 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using R7.Dnn.Extensions.ViewModels;
 using R7.University.Models;
+using R7.University.ViewModels;
 
-namespace R7.University.ViewModels
+namespace R7.University.Controls.ViewModels
 {
     public abstract class EditModelBase<TModel>: IEditModel<TModel>
     {
