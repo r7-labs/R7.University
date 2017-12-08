d90a66829d7321ef295126f6b2da358007f699ae
roman-yagodin <roman.yagodin@gmail.com>
2017-12-08 11:32:56 +0300

    [WIP] Implement mode for admission info GH-213

--- R7.University.EduProgramProfiles/Models/ContingentDirectorySettings.cs
+++ R7.University.EduProgramProfiles/Models/ContingentDirectorySettings.cs
@@ -38,7 +38,7 @@ namespace R7.University.EduProgramProfiles.Models
 
         [ModuleSetting (Prefix = "ContingentDirectory_")]
         // TODO: Default value should be null
-        public ContingentDirectoryMode? Mode { get; set; } = ContingentDirectoryMode.Actual;
+        public ContingentDirectoryMode? Mode { get; set; } = ContingentDirectoryMode.Admission;
     }
 
     public class ContingentDirectorySettingsRepository: SettingsRepository<ContingentDirectorySettings>
