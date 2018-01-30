a90b7cdd007af239e6476cbad55cbfbdd6ce5fbf
roman-yagodin <roman.yagodin@gmail.com>
2018-01-30 11:33:13 +0300

    Fix crash then editing employee achievements due to interface property attempt

--- R7.University/SerializationModels/AchievementSerializationModel.cs
+++ R7.University/SerializationModels/AchievementSerializationModel.cs
@@ -4,7 +4,7 @@
 //  Author:
 //       Roman M. Yagodin <roman.yagodin@gmail.com>
 //
-//  Copyright (c) 2017 Roman M. Yagodin
+//  Copyright (c) 2017-2018 Roman M. Yagodin
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Affero General Public License as published by
@@ -20,6 +20,7 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
+using Newtonsoft.Json;
 using R7.Dnn.Extensions.Utilities;
 using R7.University.Models;
 
@@ -39,6 +40,8 @@ namespace R7.University.SerializationModels
 
         public int AchievementID { get; set; }
 
+        [JsonIgnore]
+        [Obsolete ("Property not serialized", true)]
         public IAchievementType AchievementType { get; set; }
 
         public int? AchievementTypeId { get; set; }
