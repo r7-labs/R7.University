1edbc9eedfe4c9536ab4c8ef99be5a22f4552a67
roman-yagodin <roman.yagodin@gmail.com>
2018-03-16 12:37:18 +0300

    Bump version

--- R7.University.EduPrograms/Properties/AssemblyInfo.cs
+++ R7.University.EduPrograms/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
@@ -14,7 +14,7 @@ using System.Reflection;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("2.0.1.*")]
+[assembly: AssemblyVersion("2.1.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
