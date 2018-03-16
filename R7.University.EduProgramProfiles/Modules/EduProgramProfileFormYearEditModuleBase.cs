051e8bf5cadf50dc0424b8e94c26bc437e456e37
roman-yagodin <roman.yagodin@gmail.com>
2018-03-16 13:29:38 +0300

    Constructor in abstract class shouldn't be public

--- R7.University.EduProgramProfiles/Modules/EduProgramProfileFormYearEditModuleBase.cs
+++ R7.University.EduProgramProfiles/Modules/EduProgramProfileFormYearEditModuleBase.cs
@@ -4,7 +4,7 @@
 //  Author:
 //       Roman M. Yagodin <roman.yagodin@gmail.com>
 //
-//  Copyright (c) 2017 Roman M. Yagodin
+//  Copyright (c) 2017-2018 Roman M. Yagodin
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Affero General Public License as published by
@@ -32,7 +32,7 @@ namespace R7.University.EduProgramProfiles.Modules
 {
     public abstract class EduProgramProfileFormYearEditModuleBase<T> : UniversityEditPortalModuleBase<T> where T : class, new()
     {
-        public EduProgramProfileFormYearEditModuleBase (string key) : base (key)
+        protected EduProgramProfileFormYearEditModuleBase (string key) : base (key)
         {
         }
 
