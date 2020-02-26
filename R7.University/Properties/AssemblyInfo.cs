288c3ed3169ab0d400ff2c31d929c2155f3ad945
roman-yagodin <roman.yagodin@gmail.com>
2020-02-26 13:20:45 +0400

    Correct version

--- R7.University/Properties/AssemblyInfo.cs
+++ R7.University/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 [assembly: AssemblyVersion("2.3.0.*")]
-[assembly: AssemblyInformationalVersion("2.3-ci.1")]
+[assembly: AssemblyInformationalVersion("2.3-ci.3")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
