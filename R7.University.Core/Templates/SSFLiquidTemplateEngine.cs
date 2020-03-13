70843ffd08d6ff590295fc1b8be38233d351d2bb
roman-yagodin <roman.yagodin@gmail.com>
2020-03-13 15:26:42 +0400

    Introduce NPOI-base template support GH-305, Introduce R7.University.Core .NET Standard library project

---
+++ R7.University.Core/Templates/SSFLiquidTemplateEngine.cs
@@ -0,0 +1,142 @@
+﻿//
+//  XSSFLiquidTemplateEngine.cs
+//
+//  Author:
+//       Roman M. Yagodin <roman.yagodin@gmail.com>
+//
+//  Copyright (c) 2020 Roman M. Yagodin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using NPOI.SS.UserModel;
+
+namespace R7.University.Core.Templates
+{
+    public class SSFLiquidTemplateEngine
+    {
+        public IModelToTemplateBinder Binder;
+
+        public XSSFWorkbookProvider WorkbookProvider = new XSSFWorkbookProvider ();
+
+        public void Test ()
+        {
+            // TODO: Support {% endfor %} and multi-row loops
+            // https://github.com/tonyqus/npoi/blob/master/examples/xssf/CopySheet/Program.cs
+
+            using (var file = new FileStream ("employee_template_ru.xlsx", FileMode.Open, FileAccess.Read)) {
+
+                var templateBook = WorkbookProvider.CreateWorkbook (file);
+                var book = WorkbookProvider.CreateWorkbook ();
+                WorkbookProvider.CopyWorkbook (templateBook, book, true);
+
+                for (var s = 0; s < WorkbookProvider.GetNumberOfSheets (book); s++) {
+                    var sheet = WorkbookProvider.GetSheetAt (book, s);
+                    EvaluateObjects (sheet);
+                    EvaluateLoops (sheet);
+                    Cleanup (sheet);
+                }
+
+                WorkbookProvider.WriteWorkbook (book, new FileStream ("employee.xlsx", FileMode.Create, FileAccess.ReadWrite));
+            }
+        }
+
+        public void EvaluateObjects (ISheet sheet)
+        {
+            for (var r = sheet.FirstRowNum; r <= sheet.LastRowNum; r++) {
+                var row = sheet.GetRow (r);
+                if (row == null) {
+                    continue;
+                }
+                foreach (var cell in row.Cells) {
+                    if (IsLiquidObject (cell.StringCellValue)) {
+                        var value = Binder.Evaluate (UnwrapLiquidObject (cell.StringCellValue));
+                        if (value != null) {
+                            cell.SetCellValue (value);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void EvaluateLoops (ISheet sheet)
+        {
+            for (var r = sheet.FirstRowNum; r <= sheet.LastRowNum; r++) {
+                var row = sheet.GetRow (r);
+                if (row == null) {
+                    continue;
+                }
+                var rowIndex = 0;
+                foreach (var cell in row.Cells) {
+                    // skip affected rows
+                    if (rowIndex > row.RowNum) {
+                        continue;
+                    }
+                    var cellValue = cell.StringCellValue;
+                    if (Regex.IsMatch (cellValue, @"{%\s*for")) {
+                        var loop = LiquidLoop.Parse (UnwrapLiquidTag (cellValue));
+                        if (loop == null) {
+                            continue;
+                        }
+                        loop.NumOfRepeats = Binder.Count (loop.CollectionName);
+                        rowIndex = row.RowNum + 1;
+                        while (loop.Next ()) {
+                            WorkbookProvider.DuplicateRow (sheet, rowIndex);
+                            EvaluateRow (sheet.GetRow (rowIndex), loop);
+                            rowIndex++;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void EvaluateRow (IRow row, LiquidLoop loop)
+        {
+            foreach (var cell in row.Cells) {
+                var cellValue = cell.StringCellValue;
+                if (IsLiquidObject (cellValue)) {
+                    var objectName = UnwrapLiquidObject (cellValue);
+                    // strip loop variable name
+                    objectName = Regex.Replace (objectName, @"^" + loop.VariableName + @"\.", "");
+                    var value = Binder.Evaluate (objectName, loop.CollectionName, loop.Index);
+                    if (value != null) {
+                        cell.SetCellValue (value);
+                    }
+                }
+            }
+        }
+
+        public void Cleanup (ISheet sheet)
+        {
+            // TODO:
+        }
+
+        public string UnwrapLiquidObject (string obj)
+        {
+            return obj.TrimStart ('{').TrimEnd ('}').Trim ();
+        }
+
+        public string UnwrapLiquidTag (string obj)
+        {
+            return obj.TrimStart ('{').TrimEnd ('}').Trim ('%').Trim ();
+        }
+
+        public bool IsLiquidObject (string value)
+        {
+            return value.StartsWith ("{{", StringComparison.Ordinal) && value.EndsWith ("}}", StringComparison.Ordinal);
+        }
+    }
+}
---
+++ R7.University.Core/Templates/XSSFWorkbookProvider.cs
@@ -0,0 +1,134 @@
+//
+//  XSSFWorkbookProvider.cs
+//
+//  Author:
+//       Roman M. Yagodin <roman.yagodin@gmail.com>
+//
+//  Copyright (c) 2020 Roman M. Yagodin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.IO;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace R7.University.Core.Templates
+{
+    public class XSSFWorkbookProvider
+    {
+        public IWorkbook CreateWorkbook ()
+        {
+            return new XSSFWorkbook ();
+        }
+
+        public IWorkbook CreateWorkbook (Stream stream)
+        {
+            return new XSSFWorkbook (stream);
+        }
+
+        public void WriteWorkbook (IWorkbook book, Stream stream)
+        {
+            book.Write (stream);
+        }
+
+        public ISheet GetSheetAt (IWorkbook book, int index)
+        {
+            return book.GetSheetAt (index);
+        }
+
+        public int GetNumberOfSheets (IWorkbook book)
+        {
+            return book.NumberOfSheets;
+        }
+
+        public void CopyWorkbook (IWorkbook srcBook, IWorkbook dstBook, bool copyComments)
+        {
+            for (var i = 0; i < srcBook.NumberOfSheets; i++) {
+                var sheet = srcBook.GetSheetAt (i) as XSSFSheet;
+                sheet.CopyTo ((XSSFWorkbook) dstBook, sheet.SheetName, true, true);
+            }
+
+            if (copyComments) {
+                CopyComments (srcBook, dstBook);
+            }
+        }
+
+        void CopyComments (IWorkbook srcBook, IWorkbook dstBook)
+        {
+            for (var i = 0; i < srcBook.NumberOfSheets; i++) {
+                var srcSheet = srcBook.GetSheetAt (i) as XSSFSheet;
+                var dstSheet = dstBook.GetSheetAt (i) as XSSFSheet;
+
+                for (var r = srcSheet.FirstRowNum; r <= srcSheet.LastRowNum; r++) {
+                    var row = srcSheet.GetRow (r);
+                    if (row == null) {
+                        continue;
+                    }
+                    foreach (var srcCell in row.Cells) {
+                        if (srcCell.CellComment != null) {
+                            var dstComment = dstSheet.CreateComment ();
+                            dstComment.Author = srcCell.CellComment.Author;
+                            dstComment.Row = srcCell.CellComment.Row;
+                            dstComment.Column = srcCell.CellComment.Column;
+                            dstComment.String = srcCell.CellComment.String;
+                            dstComment.Visible = srcCell.CellComment.Visible;
+                        }
+                    }
+                }
+            }
+        }
+
+        // backported from https://stackoverflow.com/questions/6673623/npoi-insert-row-like-excel
+        public void InsertRows (ISheet sheet, int fromRowIndex, int rowCount)
+        {
+            sheet.ShiftRows (fromRowIndex, sheet.LastRowNum, rowCount);
+
+            for (var rowIndex = fromRowIndex; rowIndex < fromRowIndex + rowCount; rowIndex++) {
+                var srcRow = sheet.GetRow (rowIndex + rowCount);
+                var insRow = sheet.CreateRow (rowIndex);
+                insRow.Height = srcRow.Height;
+                // copy cells
+                for (var colIndex = 0; colIndex < srcRow.LastCellNum; colIndex++) {
+                    var srcCell = srcRow.GetCell (colIndex);
+                    var cellInsert = insRow.CreateCell (colIndex);
+                    if (srcCell != null) {
+                        cellInsert.CellStyle = srcCell.CellStyle;
+                    }
+                }
+            }
+        }
+
+        public void CopyRow (ISheet sheet, int srcRowIndex, int dstRowIndex)
+        {
+            var srcRow = sheet.GetRow (srcRowIndex);
+            var dstRow = sheet.GetRow (dstRowIndex);
+            // copy cells
+            for (var colIndex = 0; colIndex < srcRow.LastCellNum; colIndex++) {
+                var srcCell = srcRow.GetCell (colIndex);
+                var dstCell = dstRow.GetCell (colIndex);
+                if (srcCell != null && dstCell != null) {
+                    // TODO: Other value types?
+                    dstCell.SetCellValue (srcCell.StringCellValue);
+                    dstCell.CellStyle = srcCell.CellStyle;
+                }
+            }
+        }
+
+        public void DuplicateRow (ISheet sheet, int rowIndex)
+        {
+            InsertRows (sheet, rowIndex, 1);
+            CopyRow (sheet, rowIndex + 1, rowIndex);
+        }
+    }
+}
