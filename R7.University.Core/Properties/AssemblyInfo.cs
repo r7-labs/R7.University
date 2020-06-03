9c07104898a39a9c5ff1695e014e4ec9fc3a488c
roman-yagodin <roman.yagodin@gmail.com>
2020-06-03 09:39:18 +0400

    Introduce shared SolutionInfo.cs

---
+++ R7.University.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle ("R7.University.Core")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCulture ("")]
---
+++ R7.University/Properties/SolutionInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany ("R7.Labs")]
+[assembly: AssemblyProduct ("R7.University")]
+[assembly: AssemblyCopyright ("Roman M. Yagodin")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyVersion ("2.4.0")]
+[assembly: AssemblyInformationalVersion ("2.4-beta.2")]
