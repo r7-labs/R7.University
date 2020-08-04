0367bee57ac921efb51cf6b3be1313af680cd423
roman-yagodin <roman.yagodin@gmail.com>
2020-08-04 15:41:09 +0400

    Simple console menu for visual "tests"

--- R7.University.Core.Tests/Program.cs
+++ R7.University.Core.Tests/Program.cs
@@ -7,6 +7,19 @@ namespace R7.University.Core.Tests
     {
         static void Main (string [] args)
         {
+            Console.WriteLine ("Please enter test number and press [Enter]:");
+            Console.WriteLine ("1. Workbook Serialization");
+            Console.WriteLine ("0. Exit");
+
+            if (int.TryParse (Console.ReadLine (), out int testNum)) {
+                switch (testNum) {
+                    case 1: WorkbookSerialization (); break;
+                }
+            }
+        }
+
+        static void WorkbookSerialization ()
+        {
             var workbookManager = new WorkbookManager ();
             Console.Write (workbookManager.SerializeWorkbook ("./assets/templates/workbook-1.xls", WorkbookSerializationFormat.CSV));
         }
