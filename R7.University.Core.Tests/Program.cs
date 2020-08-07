6237e4595fc0a7f9c4c8ad144115c35be4e81d63
roman-yagodin <roman.yagodin@gmail.com>
2020-08-07 12:02:24 +0400

    Improve console menu for "visual" tests

--- R7.University.Core.Tests/Program.cs
+++ R7.University.Core.Tests/Program.cs
@@ -7,29 +7,40 @@ namespace R7.University.Core.Tests
     {
         static void Main (string [] args)
         {
-            Console.WriteLine ("Please enter test number and press [Enter]:");
-            Console.WriteLine ("1. Workbook to CSV");
-            Console.WriteLine ("2. Workbook to linear CSV");
-            Console.WriteLine ("0. Exit");
+            while (true) {
+                Console.WriteLine ("> Please enter test number and press [Enter]:");
+                Console.WriteLine ("---");
+                Console.WriteLine ("1. Workbook to CSV");
+                Console.WriteLine ("2. Workbook to linear CSV");
+                Console.WriteLine ("0. Exit");
 
-            if (int.TryParse (Console.ReadLine (), out int testNum)) {
-                switch (testNum) {
-                    case 1: WorkbookToCsv (); break;
-                    case 2: WorkbookToLinearCsv (); break;
+                if (int.TryParse (Console.ReadLine (), out int testNum)) {
+                    switch (testNum) {
+                        case 1: WorkbookToCsv (); break;
+                        case 2: WorkbookToLinearCsv (); break;
+                        case 0: return;
+                    }
+                    Console.WriteLine ("> Press any key to continue...");
+                    Console.ReadKey ();
+                    Console.WriteLine ();
                 }
             }
         }
 
         static void WorkbookToCsv ()
         {
+            Console.WriteLine ("--- Start test output");
             var workbookManager = new WorkbookManager ();
             Console.Write (workbookManager.SerializeWorkbook ("./assets/templates/workbook-1.xls", WorkbookSerializationFormat.CSV));
+            Console.WriteLine ("--- End test output");
         }
 
         static void WorkbookToLinearCsv ()
         {
+            Console.WriteLine ("--- Start test output");
             var workbookManager = new WorkbookManager ();
             Console.Write (workbookManager.SerializeWorkbook ("./assets/templates/workbook-1.xls", WorkbookSerializationFormat.LinearCSV));
+            Console.WriteLine ("--- End test output");
         }
     }
 }
