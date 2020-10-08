d09b595b6f62bffb507ac108bbb64215bea26441
roman-yagodin <roman.yagodin@gmail.com>
2020-10-08 16:02:43 +0400

    Data export: Export EmployeeID GH-372

--- R7.University/Templates/EmployeeModelToTemplateBinder.cs
+++ R7.University/Templates/EmployeeModelToTemplateBinder.cs
@@ -1,24 +1,3 @@
-//
-//  EmployeeModelToTemplateBinder.cs
-//
-//  Author:
-//       Roman M. Yagodin <roman.yagodin@gmail.com>
-//
-//  Copyright (c) 2020 Roman M. Yagodin
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Affero General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Affero General Public License for more details.
-//
-//  You should have received a copy of the GNU Affero General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -97,6 +76,7 @@ namespace R7.University.Templates
             AddBinding (() => Model.WorkingPlace);
             AddBinding (() => Model.WorkingHours);
 
+            AddBinding (NameOf (() => Model.EmployeeID), () => Model.EmployeeID.ToString ());
             AddBinding (NameOf (() => Model.ScienceIndexAuthorId), () => Model.ScienceIndexAuthorId.ToString ());
             AddBinding (NameOf (() => Model.ExperienceYears), () => Model.ExperienceYears.ToString ());
             AddBinding (NameOf (() => Model.ExperienceYearsBySpec), () => Model.ExperienceYearsBySpec.ToString ());
