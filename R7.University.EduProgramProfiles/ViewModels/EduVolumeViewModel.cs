c3202ff37aa61d00d0d4b58e2c24ed725c1b8ce3
roman-yagodin <roman.yagodin@gmail.com>
2020-11-11 11:59:36 +0400

    Rename props GH-328

--- R7.University.EduProgramProfiles/ViewModels/EduVolumeViewModel.cs
+++ R7.University.EduProgramProfiles/ViewModels/EduVolumeViewModel.cs
@@ -1,25 +1,4 @@
-﻿//
-//  EduVolumeViewModel.cs
-//
-//  Author:
-//       Roman M. Yagodin <roman.yagodin@gmail.com>
-//
-//  Copyright (c) 2017-2018 Roman M. Yagodin
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Affero General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Affero General Public License for more details.
-//
-//  You should have received a copy of the GNU Affero General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-using System;
+﻿using System;
 using System.Web;
 using DotNetNuke.Common.Utilities;
 using R7.Dnn.Extensions.ViewModels;
@@ -81,7 +60,7 @@ namespace R7.University.EduProgramProfiles.ViewModels
         public string CssClass =>
             this.IsPublished (HttpContext.Current.Timestamp) ? string.Empty : "u8y-not-published";
 
-        public string EduProgramProfileTitle => FormYear.EduProfile.FormatTitle (withEduProgramCode: false)
+        public string EduProfileTitle => FormYear.EduProfile.FormatTitle (withEduProgramCode: false)
                                                         .Append (FormYear.EduProfile.IsAdopted ? Context.LocalizeString ("IsAdopted.Text") : null, " - ");
 
         public string Year1Cu => UniversityFormatHelper.ValueOrDash (FormYear.EduVolume?.Year1Cu);
