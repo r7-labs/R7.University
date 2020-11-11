a44c3bbfb107f65d834d4570df7f312f5d6323bc
roman-yagodin <roman.yagodin@gmail.com>
2020-11-11 11:34:01 +0400

    Rename method GH-328

--- R7.University.EduPrograms/Queries/EduProgramQuery.cs
+++ R7.University.EduPrograms/Queries/EduProgramQuery.cs
@@ -1,25 +1,4 @@
-﻿//
-//  EduProgramQuery.cs
-//
-//  Author:
-//       Roman M. Yagodin <roman.yagodin@gmail.com>
-//
-//  Copyright (c) 2016-2018 Roman M. Yagodin
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Affero General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Affero General Public License for more details.
-//
-//  You should have received a copy of the GNU Affero General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-using System.Linq;
+﻿using System.Linq;
 using R7.Dnn.Extensions.Models;
 using R7.University.Models;
 using R7.University.Queries;
@@ -36,7 +15,7 @@ namespace R7.University.EduPrograms.Queries
         {
             return ModelContext.QueryWhere<EduProgramInfo> (ep => ep.EduProgramID == eduProgramId)
                                .IncludeEduLevelDivisionsAndDocuments ()
-                               .IncludeEduProgramProfiles ()
+                               .IncludeEduProfiles ()
                                // FIXME: Should be just SingleOrDefault: https://github.com/aspnet/EntityFrameworkCore/issues/11516
                                .ToList ()
                                .SingleOrDefault ();
--- R7.University/Queries/EduProgramQueryableExtensions.cs
+++ R7.University/Queries/EduProgramQueryableExtensions.cs
@@ -1,24 +1,3 @@
-//
-//  EduProgramQueryableExtensions.cs
-//
-//  Author:
-//       Roman M. Yagodin <roman.yagodin@gmail.com>
-//
-//  Copyright (c) 2018 Roman M. Yagodin
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Affero General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Affero General Public License for more details.
-//
-//  You should have received a copy of the GNU Affero General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using R7.University.Models;
@@ -46,7 +25,7 @@ namespace R7.University.Queries
                               .IncludeDocuments ();
         }
 
-        public static IQueryable<EduProgramInfo> IncludeEduProgramProfiles (this IQueryable<EduProgramInfo> eduPrograms)
+        public static IQueryable<EduProgramInfo> IncludeEduProfiles (this IQueryable<EduProgramInfo> eduPrograms)
         {
             return eduPrograms.Include (ep => ep.EduProfiles)
                                 .ThenInclude (epp => epp.EduLevel)
