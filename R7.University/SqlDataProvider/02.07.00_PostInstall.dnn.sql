254fd8727bbbe3d01c045aa3d1e9f1f290371757
roman-yagodin <roman.yagodin@gmail.com>
2020-12-04 14:58:49 +0400

    Set proper EduLevelID based on TitleSuffix, clear TitleSuffix GH-398

--- R7.University/SqlDataProvider/02.07.00_PostInstall.dnn.sql
+++ R7.University/SqlDataProvider/02.07.00_PostInstall.dnn.sql
@@ -2,5 +2,44 @@
 -- defaults is "dbo." for {databaseOwner} and "" for {objectQualifier}
 
 -- Assume all previously added edu. standards are state ones GH-401
+
 UPDATE {databaseOwner}[{objectQualifier}University_DocumentTypes] SET [Type] = N'StateEduStandard' WHERE [Type] = N'EduStandard'
+GO
+
+-- Set proper EduLevelID based on TitleSuffix, clear TitleSuffix GH-398
+
+UPDATE {databaseOwner}[{objectQualifier}University_EmployeeAchievements]
+    SET EduLevelID = (select EduLevelID from {databaseOwner}[{objectQualifier}University_EduLevels] where ShortTitle = N'Бакалавриат'),
+        TitleSuffix = N''
+    WHERE TitleSuffix = N'(%бакалавр%)'
+GO
+
+UPDATE {databaseOwner}[{objectQualifier}University_EmployeeAchievements]
+    SET EduLevelID = (select EduLevelID from {databaseOwner}[{objectQualifier}University_EduLevels] where ShortTitle = N'Специалитет СПО'),
+        TitleSuffix = N''
+    WHERE TitleSuffix LIKE N'(%средне%)'
+GO
+
+UPDATE {databaseOwner}[{objectQualifier}University_EmployeeAchievements]
+    SET EduLevelID = (select EduLevelID from {databaseOwner}[{objectQualifier}University_EduLevels] where ShortTitle = N'Специалитет ВО'),
+        TitleSuffix = N''
+    WHERE TitleSuffix = N'(%специал%)'
+GO
+
+UPDATE {databaseOwner}[{objectQualifier}University_EmployeeAchievements]
+    SET EduLevelID = (select EduLevelID from {databaseOwner}[{objectQualifier}University_EduLevels] where ShortTitle = N'Магистратура'),
+        TitleSuffix = N''
+    WHERE TitleSuffix = N'(%магистр%)'
+GO
+
+UPDATE {databaseOwner}[{objectQualifier}University_EmployeeAchievements]
+    SET EduLevelID = (select EduLevelID from {databaseOwner}[{objectQualifier}University_EduLevels] where ShortTitle = N'Ординатура'),
+        TitleSuffix = N''
+    WHERE TitleSuffix = N'(%ординат%)'
+GO
+
+UPDATE {databaseOwner}[{objectQualifier}University_EmployeeAchievements]
+    SET EduLevelID = (select EduLevelID from {databaseOwner}[{objectQualifier}University_EduLevels] where ShortTitle = N'Аспирантура'),
+        TitleSuffix = N''
+    WHERE TitleSuffix = N'(%кадр%)'
 GO
