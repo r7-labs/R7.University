2415201cb611745a8556c4410695289b0d517a8c
roman-yagodin <roman.yagodin@gmail.com>
2021-03-23 11:23:05 +0300

    Rename vars

--- R7.University/Commands/UpdateEduProgramDivisionCommand.cs
+++ R7.University/Commands/UpdateEduProgramDivisionCommand.cs
@@ -1,24 +1,3 @@
-//
-//  UpdateEduProgramDivisionsCommand.cs
-//
-//  Author:
-//       Roman M. Yagodin <roman.yagodin@gmail.com>
-//
-//  Copyright (c) 2017-2018 Roman M. Yagodin
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Affero General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Affero General Public License for more details.
-//
-//  You should have received a copy of the GNU Affero General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 using System.Collections.Generic;
 using R7.Dnn.Extensions.Models;
 using R7.University.EditModels;
@@ -36,21 +15,21 @@ namespace R7.University.Commands
             ModelContext = modelContext;
         }
 
-        public void Update (IEnumerable<IEditModel<EduProgramDivisionInfo>> epDocs, ModelType modelType, int itemId)
+        public void Update (IEnumerable<IEditModel<EduProgramDivisionInfo>> epDivs, ModelType modelType, int itemId)
         {
-            foreach (var epDoc in epDocs) {
-                var epd = epDoc.CreateModel ();
-                switch (epDoc.EditState) {
-                case ModelEditState.Added:
-                    epd.SetModelId (modelType, itemId);
-                    ModelContext.Add (epd);
-                    break;
-                case ModelEditState.Modified:
-                    ModelContext.UpdateExternal (epd);
-                    break;
-                case ModelEditState.Deleted:
-                    ModelContext.RemoveExternal (epd);
-                    break;
+            foreach (var epDiv in epDivs) {
+                var epd = epDiv.CreateModel ();
+                switch (epDiv.EditState) {
+                    case ModelEditState.Added:
+                        epd.SetModelId (modelType, itemId);
+                        ModelContext.Add (epd);
+                        break;
+                    case ModelEditState.Modified:
+                        ModelContext.UpdateExternal (epd);
+                        break;
+                    case ModelEditState.Deleted:
+                        ModelContext.RemoveExternal (epd);
+                        break;
                 }
             }
         }
