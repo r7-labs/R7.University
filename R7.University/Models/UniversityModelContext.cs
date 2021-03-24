6b76f7b4f032c95461d3d0281b2b4ec63edb91ce
roman-yagodin <roman.yagodin@gmail.com>
2021-03-24 14:18:36 +0300

    Rename var

--- R7.University/Models/UniversityModelContext.cs
+++ R7.University/Models/UniversityModelContext.cs
@@ -1,24 +1,3 @@
-//
-//  UniversityModelContext.cs
-//
-//  Author:
-//       Roman M. Yagodin <roman.yagodin@gmail.com>
-//
-//  Copyright (c) 2016-2018 Roman M. Yagodin
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Affero General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Affero General Public License for more details.
-//
-//  You should have received a copy of the GNU Affero General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 using DotNetNuke.Common.Utilities;
 using R7.Dnn.Extensions.Data;
 using R7.Dnn.Extensions.Models;
@@ -43,15 +22,12 @@ namespace R7.University.Models
             return UniversityDataContextFactory.Instance.Create ();
         }
 
-        public override bool SaveChanges (bool dispose = true)
+        public override bool SaveChanges (bool isFinal = true)
         {
-            var result = base.SaveChanges (dispose);
-
-            // drop cache on final call
-            if (dispose) {
+            var result = base.SaveChanges (isFinal);
+            if (isFinal) {
                 DataCache.ClearCache ("//r7_University");
             }
-
             return result;
         }
 
