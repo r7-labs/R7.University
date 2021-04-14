7465f8bb728568eb9ce910475f4c267b487e40e7
roman-yagodin <roman.yagodin@gmail.com>
2021-04-14 15:19:57 +0300

    Move rendering code closer to the view, degeneralize

--- R7.University.EduPrograms/ViewModels/EduProgramWithDocumentsViewModelBase.cs
+++ R7.University.EduPrograms/ViewModels/EduProgramWithDocumentsViewModelBase.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Web;
+using R7.Dnn.Extensions.Text;
 using R7.Dnn.Extensions.ViewModels;
 using R7.University.ModelExtensions;
 using R7.University.Models;
+using R7.University.Utilities;
 using R7.University.ViewModels;
 
 namespace R7.University.EduPrograms.ViewModels
@@ -22,15 +26,55 @@ namespace R7.University.EduPrograms.ViewModels
 
         protected string FormatDocumentLinks (IEnumerable<IDocument> documents, string microdata)
         {
-            return UniversityFormatHelper.FormatDocumentLinks (
+            return FormatDocumentLinks (
                 documents,
-                Context,
+                "<ul class=\"list-inline\">{0}</ul>",
                 "<li class=\"list-inline-item\">{0}</li>",
-                "<ul class=\"list-inline\">{0}</ul>",
-                "<ul class=\"list-inline\">{0}</ul>",
-                microdata,
-                DocumentGroupPlacement.InTitle
+                microdata
             );
+        }
+
+        public string FormatDocumentLinks (IEnumerable<IDocument> documents, string listTemplate, string itemTemplate, string microdata)
+        {
+            var markupBuilder = new StringBuilder ();
+            foreach (var document in documents) {
+                var linkMarkup = FormatDocumentLink (document,
+                    document.Title,
+                    Context.LocalizeString ("LinkOpen.Text"),
+                    microdata,
+                    HttpContext.Current.Timestamp
+                );
+
+                if (!string.IsNullOrEmpty (linkMarkup)) {
+                    markupBuilder.Append (string.Format (itemTemplate, linkMarkup));
+                }
+            }
+
+            var markup = markupBuilder.ToString ();
+            if (!string.IsNullOrEmpty (markup)) {
+                return string.Format (listTemplate, markup);
+            }
+
+            return string.Empty;
+        }
+
+        string FormatDocumentLink (IDocument document, string documentTitle,
+            string defaultTitle, string microdata, DateTime now)
+        {
+            var title = !string.IsNullOrWhiteSpace (documentTitle)
+                ? FormatHelper.JoinNotNullOrEmpty (": ", document.Group, documentTitle)
+                : defaultTitle;
+
+            if (!string.IsNullOrWhiteSpace (document.Url)) {
+                var linkMarkup = "<a href=\"" +
+                                 UniversityUrlHelper.LinkClick (document.Url, Context.Module.TabId, Context.Module.ModuleId) + "\" "
+                                 + FormatHelper.JoinNotNullOrEmpty (" ", !document.IsPublished (now) ? "class=\"u8y-not-published-element\"" : string.Empty, microdata)
+                                 + " target=\"_blank\">" + title + "</a>";
+
+                return linkMarkup;
+            }
+
+            return string.Empty;
         }
     }
 }
