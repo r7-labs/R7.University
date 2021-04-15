4740b12145a79a847c477f72854edc2ecafb2939
roman-yagodin <roman.yagodin@gmail.com>
2021-04-15 15:45:06 +0300

    Nicer view #412

--- R7.University.EduPrograms/ViewModels/EduProgramWithDocumentsViewModelBase.cs
+++ R7.University.EduPrograms/ViewModels/EduProgramWithDocumentsViewModelBase.cs
@@ -2,11 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Web;
-using DotNetNuke.Services.FileSystem;
 using R7.Dnn.Extensions.Text;
 using R7.Dnn.Extensions.ViewModels;
 using R7.University.ModelExtensions;
 using R7.University.Models;
+using R7.University.UI;
 using R7.University.Utilities;
 using R7.University.ViewModels;
 
@@ -67,18 +67,24 @@ namespace R7.University.EduPrograms.ViewModels
                 : defaultTitle;
 
             if (!string.IsNullOrWhiteSpace (document.Url)) {
-                var linkMarkup = "<a href=\"" +
-                                 UniversityUrlHelper.LinkClick (document.Url, Context.Module.TabId, Context.Module.ModuleId) + "\" "
-                                 + FormatHelper.JoinNotNullOrEmpty (" ", !document.IsPublished (now) ? "class=\"u8y-not-published-element\"" : string.Empty, microdata)
-                                 + " target=\"_blank\">" + title + "</a>";
+                var fa = FontAwesomeHelper.Instance;
+                var documentFile = UniversityFileHelper.Instance.GetFileByUrl (document.Url);
 
-                var sigFile =
-                    UniversityFileHelper.Instance.GetSignatureFile (
-                        UniversityFileHelper.Instance.GetFileByUrl (document.Url));
+                var linkMarkup =
+                    $"<i class=\"fas fa-file-{fa.GetBaseIconNameByExtension(documentFile.Extension)}\""
+                    + $"style=\"color:{fa.GetBrandColorByExtension(documentFile.Extension)}\"></i>"
+                    + $" <a href=\"{UniversityUrlHelper.LinkClick (document.Url, Context.Module.TabId, Context.Module.ModuleId)}\" "
+                    + FormatHelper.JoinNotNullOrEmpty (" ", !document.IsPublished (now) ? "class=\"u8y-not-published-element\"" : string.Empty, microdata)
+                    + $" target=\"_blank\">{title}</a>";
+
+                var sigFile = UniversityFileHelper.Instance.GetSignatureFile (documentFile);
 
                 if (sigFile != null) {
-                    linkMarkup += "<span> | </span><a href=\"" + UniversityUrlHelper.LinkClickFile (sigFile.FileId,
-                        Context.Module.TabId, Context.Module.ModuleId) + "\">.sig</a>";
+                    linkMarkup += "<span> | </span>"
+                                  + $"<a href=\"{UniversityUrlHelper.LinkClickFile (sigFile.FileId, Context.Module.TabId, Context.Module.ModuleId)}\" "
+                                  + $"title=\"{Context.LocalizeString ("Signature.Text")}\"><i class=\"fas fa-signature\"></i></a>";
+
+
                 }
 
                 return linkMarkup;
