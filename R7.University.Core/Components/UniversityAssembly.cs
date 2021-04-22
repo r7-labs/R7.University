b551e8033bb11ec4c9439613533b9a29bebbbb50
roman-yagodin <roman.yagodin@gmail.com>
2021-04-22 10:47:19 +0300

    Move class

---
+++ R7.University.Core/Components/UniversityAssembly.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace R7.University.Components
+{
+    public static class UniversityAssembly
+    {
+        public static Assembly GetCoreAssembly ()
+        {
+            return Assembly.GetExecutingAssembly ();
+        }
+
+        public static string SafeGetInformationalVersion (int fieldCount)
+        {
+            var coreAssembly = GetCoreAssembly ();
+            var attr = coreAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute> ();
+            if (attr != null) {
+                return attr.InformationalVersion;
+            }
+            return coreAssembly.GetName ().Version.ToString (fieldCount);
+        }
+    }
+}
